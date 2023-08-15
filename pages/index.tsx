f668eaf5316e5ba3f539627f857ef7e3f824b798
Rattin99 <rattin99m18@gmail.com>
2023-08-15 23:27:45 +0600

    aes done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,169 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { json } from 'node:stream/consumers';
+import {useRef, useState} from 'react';
 
 export default function Home() {
+
+  const [algo,setAlgo] = useState('AES-128-CBC')
+  const [opt,setOpt] = useState("")
+  const [file,setFile] = useState(null)
+  const [encryptedFileInfo, setEncryptedFileInfo] = useState(null)
+  const [text,setText] = useState(null)
+  const [textEncryptData,setTextEncryptData] = useState(null)
+  
+  const textAreaRef = useRef()
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("file",file,file.name,algo);
+
+    const url = `http://localhost:5000/file/${algo}`
+    const res = await fetch(url,{
+      method: 'POST',
+      body: formData
+    })
+
+    const response = await res.json();
+
+  
+    setEncryptedFileInfo(response)
+    console.log(encryptedFileInfo)
+
+  }
+  const changeHandler = (e:any) => {
+    setFile(e.target.files[0])
+  }
+
+  const selectHandler = (e:any) => {
+    setOpt(e.target.value)
+  }
+
+  const decryptHandler = async (e:any) => {
+   
+    const b = {
+      filepath: encryptedFileInfo.filePath,
+      fileName: encryptedFileInfo.fileName,
+      algo: algo
+    }
+    const res = await fetch("http://localhost:5000/dec",{
+      method: 'POST',
+      body: JSON.stringify(b),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+
+    const response = await res.json();
+
+    if(response.status === 'decryption done'){
+      setEncryptedFileInfo(null)
+    }
+  }
+
+  const algoHandler = (e:any) => {
+    setAlgo(e.target.value)
+  }
+
+  const textChangeHandler = (e:any) => {
+    setText(e.target.value)
+  }
+  const textSubmitHandler = async (e:any) => {
+    const res = await fetch("http://localhost:5000/text",{
+      method: 'POST',
+      body: JSON.stringify({
+        text,algo
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+    const response = await res.json()
+
+    textAreaRef.current.value = response.encryptedText
+
+    setTextEncryptData(response)
+    
+  }
+
+
+   const textDecryptHandler = async (e:any) => {
+    const res = await fetch("http://localhost:5000/textd",{
+      method: 'POST',
+      body: JSON.stringify(textEncryptData),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+    const response = await res.json()
+
+    textAreaRef.current.value = response.decryptedData 
+
+    setTextEncryptData(null)
+   }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className=' w-screen h-screen  flex flex-col items-center bg-black'>
+      <div className='mx-auto my-20'>
+          <select name='dropdown' className='m-auto p-2 px-24 rounded white' onChange={algoHandler} >
+            <option>AES-128-CBC</option>
+            <option>AES-128-CFB</option>
+            <option>AES-128-ECB</option>
+            <option>AES-128-OFB</option>
+            <option>DES-EDE-CBC</option>
+            <option>DES-EDE3-CFB</option>
+          </select>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='mx-auto my-20'>
+          <select name='dropdown' className='m-auto p-2 px-24 rounded white' onChange={selectHandler} >
+            <option>video</option>
+            <option>image</option>
+            <option>text</option>
+            <option>audio</option>
+          </select>
       </div>
+      {opt !== 'text' && opt !== '' && (
+         <div className='flex flex-col items-center mx-auto'>
+            <div className="flex flex-col items-center ">
+              <form className='p-10 text-white flex flex-col items-center'>
+                  <input name='file' type='file' onChange={changeHandler}/>
+              </form>
+              <button className='border bg-white text-black p-3 rounded' onClick={handleSubmit}>submit</button>
+            </div>
+          </div>
+      )}
+      {
+        opt === 'text' && (
+          <div>
+              <div className='bg-white rounded '>
+                <div>
+                  <textarea className='p-11'
+                  ref={textAreaRef} onChange={textChangeHandler}></textarea>
+                </div>
+              </div>
+              <div className='flex justify-center items-center'>
+                {
+                  textEncryptData ?(
+                    <button className='border bg-white text-black p-3 rounded my-3' onClick={textDecryptHandler}>decrypt</button>
+                  ) : 
+                  (
+                    <button className='border bg-white text-black p-3 rounded my-3'  onClick={textSubmitHandler}>submit</button>
+                  )
+                }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              </div>
+          </div>
+        )
+      }
+      {
+        encryptedFileInfo && (
+          <div>
+             <h1 className='text-white my-10'>encryption complete</h1>
+             <button className='border bg-white text-black p-3 rounded my-3' onClick={decryptHandler}>decrypt last file</button>
+          </div>
+        )
+      }
+    </div>
   )
 }
